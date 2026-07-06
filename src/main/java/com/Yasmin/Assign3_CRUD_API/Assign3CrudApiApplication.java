0e7b9cc2320e2a80ab2e815027c70310550fe36a
Yasmin <yacastrocer@uncg.edu>
2026-07-05 23:36:34 -0400

    Copied all content of SprigBoot into repo

---
+++ src/main/java/com/Yasmin/Assign3_CRUD_API/Assign3CrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.Yasmin.Assign3_CRUD_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assign3CrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assign3CrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Yasmin/Assign3_CRUD_API/Assign3CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yasmin.Assign3_CRUD_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assign3CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
