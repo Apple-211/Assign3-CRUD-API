127d733466bcff5ab46dfaddf799f97b15b11276
Yasmin <yacastrocer@uncg.edu>
2026-07-13 23:33:05 -0400

    Finished cleaning code and extra credit

--- src/main/java/com/Yasmin/Assign3_CRUD_API/Assign3CrudApiApplication.java
+++ src/main/java/com/Yasmin/Assign3_CRUD_API/Assign3CrudApiApplication.java
@@ -2,8 +2,6 @@ package com.Yasmin.Assign3_CRUD_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 public class Assign3CrudApiApplication {
