5ffc55c7e3ef3d0e228f3f64337d660ab9a9cb4c
Yasmin <yacastrocer@uncg.edu>
2026-07-13 23:04:20 -0400

    Implemented update, create, and delete

--- src/main/java/com/Yasmin/Assign3_CRUD_API/Assign3CrudApiApplication.java
+++ src/main/java/com/Yasmin/Assign3_CRUD_API/Assign3CrudApiApplication.java
@@ -2,6 +2,8 @@ package com.Yasmin.Assign3_CRUD_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 public class Assign3CrudApiApplication {
--- src/main/java/com/Yasmin/Assign3_CRUD_API/characters/CharacterUIController.java
+++ src/main/java/com/Yasmin/Assign3_CRUD_API/characters/CharacterUIController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CharacterUIController {
@@ -37,6 +38,42 @@ public String getCharacterList(Model model) {
     model.addAttribute("characterList", characterService.getAllCharacters());
 
     return "character-list";
+}
+
+@GetMapping("/characters/create")
+public String showCreateForm() {
+    return "character-create";
+}
+
+@PostMapping("/characters/create")
+public String createCharacter(Character character) {
+    characterService.addCharacter(character);
+    return "redirect:/characters";
+}
+
+@GetMapping("/characters/update/{id}")
+public String showUpdateForm(@PathVariable Long id, Model model) {
+
+    Character character = characterService.getCharacterById(id);
+
+    model.addAttribute("character", character);
+
+    return "character-update";
+}
+
+@PostMapping("/characters/update/{id}")
+public String updateCharacter(@PathVariable Long id,
+                              Character character) {
+
+    characterService.updateCharacter(id, character);
+
+    return "redirect:/characters/" + id;
+}
+
+@GetMapping("/characters/delete/{id}")
+public String deleteCharacter(@PathVariable Long id) {
+    characterService.deleteCharacter(id);
+    return "redirect:/characters";
 }
     
 }
