2a1bc44afd4a1dde6fa34510610e93a971424016
Yasmin <yacastrocer@uncg.edu>
2026-07-13 23:11:05 -0400

    Fix a small error

--- src/main/java/com/Yasmin/Assign3_CRUD_API/characters/CharacterUIController.java
+++ src/main/java/com/Yasmin/Assign3_CRUD_API/characters/CharacterUIController.java
@@ -3,7 +3,7 @@ package com.Yasmin.Assign3_CRUD_API.characters;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
