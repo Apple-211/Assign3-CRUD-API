8c55eedb15fb5fb7d1b1482b64a0262691282b97
Yasmin <yacastrocer@uncg.edu>
2026-07-13 21:38:07 -0400

    Implemented freeMarker character list

--- src/main/java/com/Yasmin/Assign3_CRUD_API/characters/CharacterUIController.java
+++ src/main/java/com/Yasmin/Assign3_CRUD_API/characters/CharacterUIController.java
@@ -1,6 +1,10 @@
 package com.Yasmin.Assign3_CRUD_API.characters;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CharacterUIController {
@@ -10,5 +14,29 @@ public class CharacterUIController {
         this.characterService = characterService;
 
     }
+
+    @GetMapping("/about")
+        public String about() {
+    return "about";
+
+}
+
+@GetMapping("/characters/{id}")
+public String getCharacterDetails(@PathVariable Long id, Model model) {
+
+    Character character = characterService.getCharacterById(id);
+
+    model.addAttribute("character", character);
+
+    return "character-details";
+}
+
+@GetMapping("/characters")
+public String getCharacterList(Model model) {
+
+    model.addAttribute("characterList", characterService.getAllCharacters());
+
+    return "character-list";
+}
     
 }
